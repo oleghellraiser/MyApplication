4bc28d27c4952fd00dcff8715403c0007a4df7f7
oleghellraiser <oleghellraiser@gmail.com>
2016-03-11 21:54:59 +0300

    Create MainActivity class and Layout

---
+++ app/src/main/java/com/example/oleg/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.oleg.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ * Created by Oleg on 3/11/2016.
+ */
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
